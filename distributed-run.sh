da12703846cfc313eb0aac26a4304ca05b2283ab
punwai <punwaiw@gmail.com>
2025-02-20 16:23:38 -0800

    save all

--- distributed-run.sh
+++ distributed-run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Save as ~/scripts/distributed-run
-
 # Function to kill processes in all tmux sessions
 kill_tmux_processes() {
     echo "Sending kill signal to all tmux sessions..."
@@ -15,7 +14,6 @@ kill_tmux_processes() {
     wait
     echo "Kill signals sent to all machines."
 }
-
 # Check for kill command
 if [[ "$1" == "--kill" ]]; then
     # Set machines from argument or default
@@ -26,25 +24,22 @@ if [[ "$1" == "--kill" ]]; then
     kill_tmux_processes
     exit 0
 fi
-
 ARG1="$1"
 # Set default for second argument if not provided
 ARG2="${2:-"ttt-1 ttt-2 ttt-3 ttt-4 ttt-5 ttt-6 ttt-7 ttt-8"}"
-# First argument is space-separated machine names
-MACHINES=($ARG2)
-# Second argument is the command
+# First argument is the command, second is space-separated machine names
 COMMAND=$ARG1
-
+MACHINES=($ARG2)
 evaluate_math() {
     local expr=$1
     echo "scale=6; $expr" | bc | sed 's/^\./0./' | sed 's/^-\./-0./' | awk '{printf "%.6f\n", $0}' | sed 's/\.000000$//'
 }
-
 process_template() {
     local template=$1
     local id=$2
     local machine_name=${MACHINES[$id]}
     
+    # Process the template to replace variables
     while [[ "$template" =~ \$\{([^}]*)\} ]]; do
         local match="${BASH_REMATCH[0]}"
         local expr="${BASH_REMATCH[1]}"
@@ -66,10 +61,8 @@ process_template() {
     
     echo "$template"
 }
-
 # Get number of machines
 NUM_MACHINES=${#MACHINES[@]}
-
 # Run commands on all machines simultaneously
 for ((id=0; id<NUM_MACHINES; id++)); do
     machine_name=${MACHINES[$id]}
@@ -84,6 +77,5 @@ for ((id=0; id<NUM_MACHINES; id++)); do
 EOF
     )
 done
-
 echo "Commands sent to all machines. To kill all processes, run: $(basename $0) --kill"
 wait
